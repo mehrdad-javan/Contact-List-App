37f74559778d3cebac0741d3c8dbe8bd88869e5a
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-06-24 10:23:26 +0200

    implement the add method of contact repository

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,17 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import javax.naming.Name;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println(ContactRepository.size()); // 0
+        boolean isAddedContact1 = ContactRepository.add("Test,1234567890,test@test.se");
+        System.out.println(isAddedContact1); // -> true
+        System.out.println(ContactRepository.size()); // 1
+        System.out.println(ContactRepository.add("Test2,1234567890,test2@test2.se")); // ? false
+        System.out.println(ContactRepository.size()); // 1
+
     }
 }
---
+++ src/main/java/se/lexicon/ContactRepository.java
@@ -0,0 +1,48 @@
+package se.lexicon;
+
+import java.util.Arrays;
+
+public class ContactRepository {
+
+    private static final int MAX_SIZE = 100;
+
+    private static String[] contacts = new String[0];
+
+    // Methods
+    public static boolean add(String contact) { // "Name,Mobile,Email"
+        if (contact == null || contact.isEmpty()) {
+            //throw new IllegalArgumentException("Contact  Data is null.");
+            return false;
+        }
+        if (contacts.length > MAX_SIZE) {
+            //throw new IllegalArgumentException("Contact length should not be > " + MAX_SIZE);
+            return false;
+        }
+        if (isDuplicateMobileNumber(contact)) {
+            //throw new IllegalArgumentException("Contact  Data is null.");
+            return false;
+        }
+        String[] newArray = Arrays.copyOf(contacts, contacts.length + 1);
+        newArray[newArray.length - 1] = contact;
+        contacts = newArray;
+        return true;
+    }
+
+    private static boolean isDuplicateMobileNumber(String contact) { //"Name,Mobile,Email"
+        String paramMobileNumber = contact.split(",")[1];
+        for (String contactElement : contacts) { // "name,mobile,email"
+            String elementMobileNumber = contactElement.split(",")[1];
+            if (elementMobileNumber.equals(paramMobileNumber)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static int size(){
+        return contacts.length;
+    }
+
+
+}
